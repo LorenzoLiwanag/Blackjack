f055fbfef668b5ad20d149c7c8602ef6c5f963c4
LorenzoLiwanag <lorenzonlliwanag@outlook.com>
2024-11-05 19:09:08 -0700

    trying to implement numbered card set value

--- Blackjack/Card.cpp
+++ Blackjack/Card.cpp
@@ -1,4 +1,6 @@
 #include "Card.h"
+#include <iostream>
+#include <ctime>
 
 
 int FaceCard::setValue()
@@ -6,16 +8,19 @@ int FaceCard::setValue()
 	int faceCardValue = 10;
 	return faceCardValue;
 }
-
+//generate a random number. that random number will determine which element in the numbered cards array will be used. that is what will also be returned.
 int NumberedCard::setValue()
 {
 	int numberedCards[9] { 2,3,4,5,6,7,8,9,10 };
+	srand(time(0));
+	int selectedNumberedCard = rand() % 9 + 2;
 
 }
 
 int AceCard::setValue()
 {
 	int playerChooses1Or11;
+	std::cin >> playerChooses1Or11;
 	int aceValue;
 	playerChooses1Or11 == 1 ? aceValue = 1 : aceValue = 11;
 	return aceValue;
